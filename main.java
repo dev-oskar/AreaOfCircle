41566681bbfc9deb50e2f26d96a28feea1c42759
Oskar Pietrucha <velho.oscar@outlook.com>
2017-09-19 12:45:13 +0200

    Updated PL and ENG translations

--- main.java
+++ main.java
@@ -6,31 +6,28 @@ public class main {
     public static void main(String[] args) {
 
         int radius = 0;
-        System.out.println("Prosze wpisac promien okregu: ");
+        System.out.println("Prosze wpisac promien okregu: "); //Please enter radius of the circle
 
         try{
             //Pobierz promień od użytkownika
+            //Get radius value from user
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
             radius = Integer.parseInt(br.readLine());
         }
         //Jeśli została wprowadzona niepoprawna liczba
+        //If invalid value was entered
         catch(NumberFormatException ne){
-            System.out.println("Zostala wprowadzona niepoprawna liczba" + ne);
+            System.out.println("Zostala wprowadzona niepoprawna liczba" + ne); //Invalid radius value
             System.exit(0);
         }
         catch (IOException ioe){
-            System.out.println("Blad IO: " + ioe);
+            System.out.println("Blad IO: " + ioe); //IO ERROR
             System.exit(0);
         }
-        /*
-        Pole okręgu liczy się ze wzoru:
-        Pi * promień * promień (P*r^2)
-        Liczba PI wyciągnięta z Math.PI
-         */
-
         double area = Math.PI * radius * radius;
-
-        System.out.println("Pole kola wynosi :" + area);
+        
+        //Area of the circle is
+        System.out.println("Pole kola wynosi :" + area); 
     }
 }
